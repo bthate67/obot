5b2fe37f45e7bef4bb00c5acd3e423b9295b3c4c
Bart Thate <bthate67@gmail.com>
2021-04-24 17:59:00 +0200

    add table

---
+++ obot/tbl.py
@@ -0,0 +1,114 @@
+# This file is placed in the Public Domain.
+
+table = {
+    "inits": {
+        "adm": "obot.adm",
+        "irc": "obot.irc",
+        "log": "obot.log",
+        "mbx": "obot.mbx",
+        "rss": "obot.rss",
+        "tbl": "obot.tbl",
+        "tdo": "obot.tdo",
+        "udp": "obot.udp"
+    },
+    "modules": {
+        "cfg": "obot.irc",
+        "cmd": "obot.cmd",
+        "dlt": "obot.irc",
+        "dne": "obot.tdo",
+        "dpl": "obot.rss",
+        "flt": "obot.adm",
+        "ftc": "obot.rss",
+        "krn": "obot.adm",
+        "log": "obot.log",
+        "mbx": "obot.mbx",
+        "met": "obot.irc",
+        "rem": "obot.rss",
+        "rss": "obot.rss",
+        "tdo": "obot.tdo",
+        "thr": "obot.adm",
+        "upt": "obot.adm"
+    },
+    "names": {
+        "bus": [
+            "bus.Bus"
+        ],
+        "cfg": [
+            "obot.rss.Cfg",
+            "obot.irc.Cfg",
+            "obot.udp.Cfg"
+        ],
+        "client": [
+            "hdl.Client"
+        ],
+        "dcc": [
+            "obot.irc.DCC"
+        ],
+        "default": [
+            "obj.Default"
+        ],
+        "email": [
+            "obot.mbx.Email"
+        ],
+        "enouser": [
+            "err.ENOUSER"
+        ],
+        "event": [
+            "obot.irc.Event"
+        ],
+        "feed": [
+            "obot.rss.Feed"
+        ],
+        "fetcher": [
+            "obot.rss.Fetcher"
+        ],
+        "handler": [
+            "hdl.Handler"
+        ],
+        "httperror": [
+            "urllib.error.HTTPError"
+        ],
+        "irc": [
+            "obot.irc.IRC"
+        ],
+        "log": [
+            "obot.log.Log"
+        ],
+        "names": [
+            "nms.Names"
+        ],
+        "object": [
+            "obj.Object"
+        ],
+        "output": [
+            "opt.Output"
+        ],
+        "repeater": [
+            "clk.Repeater"
+        ],
+        "rss": [
+            "obot.rss.Rss"
+        ],
+        "seen": [
+            "obot.rss.Seen"
+        ],
+        "textwrap": [
+            "obot.irc.TextWrap"
+        ],
+        "todo": [
+            "obot.tdo.Todo"
+        ],
+        "udp": [
+            "obot.udp.UDP"
+        ],
+        "urlerror": [
+            "urllib.error.URLError"
+        ],
+        "user": [
+            "obot.irc.User"
+        ],
+        "users": [
+            "obot.irc.Users"
+        ]
+    }
+}
