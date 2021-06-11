1fdc5d8d8dfe9139b6b485727410fb2e1568dfdd
Bart Thate <bthate@dds.nl>
2021-06-11 09:24:48 +0200

    add error module

---
+++ olib/err.py
@@ -0,0 +1,25 @@
+# This file is placed in the Public Domain.
+
+class NoClassError(Exception):
+
+    pass
+
+class NoTypeError(Exception):
+
+    pass
+
+class NoTextError(Exception):
+
+    pass
+
+class NoUserError(Exception):
+
+    pass
+
+class RestartError(Exception):
+
+    pass
+
+class StopError(Exception):
+
+    pass
